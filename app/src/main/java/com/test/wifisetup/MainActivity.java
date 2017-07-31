19bec201b59d92b3e9bb4e6cf62f8b48bda0852d
sky <sky.cheng@harman.com>
2017-07-31 10:25:15 +0800

    add edit text to input said and password

--- app/src/main/java/com/test/wifisetup/MainActivity.java
+++ app/src/main/java/com/test/wifisetup/MainActivity.java
@@ -2,19 +2,18 @@ package com.test.wifisetup;
 
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.test.wifisetup.R;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -32,6 +31,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private AtomicInteger failCnt = new AtomicInteger();
 
     private String connectingSSID = null;
+    private String mSSID = null;
+    private String mPwd = null;
+    private String mSpeaker = null;
+    private EditText mEditTextSSID = null;
+    private EditText mEditTextPwd = null;
+    private EditText mEditTextSpeaker = null;
 
 
     private Handler handler = new Handler(){
@@ -48,12 +53,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void autoConnect(int what){
         if(what == 0){
-            connectingSSID = "HK_Omni_10+_Setup_ffd";
+            connectingSSID = mSpeaker;
             WifiConnectionManager wifi0 = new WifiConnectionManager(this, connectingSSID, "");
             wifi0.connectToWifi();
         }else{
-            connectingSSID = "D-Link_DIR-816_5G";
-            WifiConnectionManager wifi1 = new WifiConnectionManager(this, connectingSSID, "12345678");
+            connectingSSID = mSSID;
+            WifiConnectionManager wifi1 = new WifiConnectionManager(this, connectingSSID, mPwd);
             wifi1.connectToWifi();
         }
         Log.d(TAG, "connecting..............................................." + connectingSSID);
@@ -65,6 +70,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditTextSSID = (EditText) findViewById(R.id.ssid);
+        mEditTextPwd = (EditText) findViewById(R.id.password);
+        mEditTextSpeaker = (EditText) findViewById(R.id.speaker_ssid);
 
         wifiReceiver = new WifiNetworkStateReceiver(new OnNetworkStateChangeListener() {
             @Override
@@ -104,15 +112,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        mSSID = mEditTextSSID.getText().toString();
+        mPwd = mEditTextPwd.getText().toString();
+        mSpeaker = mEditTextSpeaker.getText().toString();
         switch(view.getId()){
             case R.id.speaker:
                 Log.d(TAG, "click speaker.....");
-                WifiConnectionManager wifi0 = new WifiConnectionManager(this, "HK_Omni_10+_Setup_ffd", "");
+                WifiConnectionManager wifi0 = new WifiConnectionManager(this, mSpeaker, "");
                 wifi0.connectToWifi();
                 break;
             case R.id.dlink:
-                Log.d(TAG, "click dlink.....");
-                WifiConnectionManager wifi1 = new WifiConnectionManager(this, "D-Link_DIR-816_5G", "12345678");
+                Log.d(TAG, "click router.....");
+                WifiConnectionManager wifi1 = new WifiConnectionManager(this, mSSID, mPwd);
                 wifi1.connectToWifi();
                 break;
             case R.id.autoTest:
